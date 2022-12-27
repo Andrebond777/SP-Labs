4e17605f29de7d9cb2f6023bca019695f9ab702a
Andrebond777 <41829674+Andrebond777@users.noreply.github.com>
2022-12-27 19:46:42 +0000

    Update lab2Task2.c

--- lab2Task2.c
+++ lab2Task2.c
@@ -1,121 +1,67 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-#define SIZEBUF (1024)
+#define BUFFERLENGTH (1024)
 #define CHMOD (0644)
 
 int main(int argc, char** argv, char** envp)
 {
 	pid_t pid;
-	int logFile;
+	int log;
 
-	if ((logFile = open("log.txt", O_WRONLY | O_CREAT | O_TRUNC, CHMOD)) == -1)
-	{
-		printf("Error with open(log.txt, ...)\n%s\n", strerror(errno));
+	if ((log = open("log.txt", O_WRONLY | O_CREAT | O_TRUNC, CHMOD)) == -1)
 		exit(1);
-	}
-
-	if ((write(logFile, "Parent process started\n", sizeof("Parent process started\n") - 1)) == -1)
-	{
-		printf("Error with write()\n%s\n", strerror(errno));
+	if ((write(log, "Begin parent proc\n", sizeof("Begin parent proc\n") - 1)) == -1)
 		exit(1);
-	}
-
 	if ((pid = fork()) < 0)
-	{
-		printf("Error with fork()\n%s\n", strerror(errno));
 		exit(1);
-	}
-	else if (pid == 0) // Child
+
+	if (pid != 0)
 	{
-		int devNull;
-		char* str;
-		size_t memNeeded;
-		char* path = malloc(SIZEBUF * sizeof * path);
-		getcwd(path, SIZEBUF);
-
-		if (setsid() == -1)
-		{
-			printf("Error with setsid()\n%s\n", strerror(errno));
+		if ((write(log, "Child proc was created by parent proc\n", sizeof("Child proc was created by parent proc\n") - 1)) == -1)
 			exit(1);
-		}
+	}
+	else
+	{
+		char* path = malloc(BUFFERLENGTH * sizeof * path);
+		getcwd(path, BUFFERLENGTH);
 
-		if (chdir("/") == -1)
-		{
-			printf("Error with chdir()\n%s\n", strerror(errno));
-			exit(1);
-		}
+		char* text;
+		int dev_null;
+		size_t memory;
 
-		if (close(logFile) == -1)
-		{
-			printf("Error with close()\n%s\n", strerror(errno));
+		if (setsid() == -1 || chdir("/") == -1 || close(log) == -1)
 			exit(1);
-		}
+		if ((dev_null = open("/dev/null", O_WRONLY)) == -1)
+			exit(1);
 
-		if ((devNull = open("/dev/null", O_WRONLY)) == -1)
-		{
-			printf("Error with open(/dev/null, ...)\n%s\n", strerror(errno));
-			exit(1);
-		}
-
-		if (dup2(devNull, 0) == -1) // stdin
-		{
-			printf("Error with dup2(devNull, 0)\n%s\n", strerror(errno));
-			exit(1);
-		}
-
-		if (dup2(devNull, 1) == -1) // stdout
-		{
-			printf("Error with dup2(devNull, 1)\n%s\n", strerror(errno));
-			exit(1);
-		}
-
-		if (dup2(devNull, 2) == -1) // stderr
-		{
-			printf("Error with dup2(devNull, 2)\n%s\n", strerror(errno));
-			exit(1);
-		}
+		for (size_t i = 0; i < 3; i++)
+			if (dup2(dev_null, i) == -1)
+				exit(1);
 
 		strcat(path, "/log.txt");
 
-		if ((logFile = open(path, O_WRONLY | O_APPEND, CHMOD)) == -1)
-		{
-			printf("Error with open()\n%s\n", strerror(errno));
+		if ((log = open(path, O_WRONLY | O_APPEND, CHMOD)) == -1)
 			exit(1);
-		}
-
-		if ((write(logFile, "Child process was created\n", sizeof("Child process was created\n") - 1)) == -1)
-		{
-			printf("Error with write()\n%s\n", strerror(errno));
+		if ((write(log, "Created child proc\n", sizeof("Created child proc\n") - 1)) == -1)
 			exit(1);
-		}
 
 		while (1)
 		{
-			memNeeded = snprintf(NULL, 0, "Child process:\npid = %d;\ngid = %d;\nsid = %d.\n\n", getpid(), getgid(), getsid(getpid())) + sizeof('\0');
-			str = malloc(memNeeded);
-			snprintf(str, memNeeded, "Child process:\npid = %d;\ngid = %d;\nsid = %d.\n\n", getpid(), getgid(), getsid(getpid()));
+			memory = snprintf(NULL, 0, "Child proc\npid - %d\nsid - %d\ngid - %d\n\n", getpid(), getsid(getpid()), getgid()) + sizeof('\0');
+			text = malloc(memory);
+			snprintf(text, memory, "Child proc\npid - %d\nsid - %d\ngid - %d\n\n", getpid(), getsid(getpid()), getgid());
 
-			if ((write(logFile, str, strlen(str))) == -1)
-			{
-				printf("Error with write()\n%s\n", strerror(errno));
+			if ((write(log, text, strlen(text))) == -1)
 				exit(1);
-			}
-			sleep(1);
-		}
-	}
-	else // Parent
-	{
-		if ((write(logFile, "Parent process created child process\n", sizeof("Parent process created child process\n") - 1)) == -1)
-		{
-			printf("Error with write()\n%s\n", strerror(errno));
-			exit(1);
+
+			sleep(2);
 		}
 	}
 
